b008905eb39de3c1c717eeaa61c77e5420b60b28
unknown <pepseman727@gmail.com>
2024-03-18 21:21:34 +0900

    Test using rustfmt

--- task9/src/main.rs
+++ task9/src/main.rs
@@ -5,24 +5,19 @@ fn main() {
     //Рассчётная формула 1/R = 1/R1 + 1/R2 + 1/R3
     // => R = R1R2R3/R2R3+R1R3+R1R2
     println!("Enter three resistance values separated by a space");
-    io::stdin()
-        .read_line(&mut resist)
-        .expect("Enter value");
+    io::stdin().read_line(&mut resist).expect("Enter value");
 
-    let resist: Vec<f64> = resist.trim()
-                            .split(' ')
-                            .map(|value| value.to_string().parse::<f64>().expect("Enter a number"))
-                            .collect();
-    
+    let resist: Vec<f64> = resist
+        .trim()
+        .split(' ')
+        .map(|value| value.to_string().parse::<f64>().expect("Enter a number"))
+        .collect();
+
     if let [r1, r2, r3] = resist.clone().as_slice() {
-            let numerator: f64 = r1 * r2 * r3;
-            let denominator = r2 * r3 + r1 * r3 + r1 * r2;
-            println!("Total resistance {}", numerator/denominator);
+        let numerator: f64 = r1 * r2 * r3;
+        let denominator = r2 * r3 + r1 * r3 + r1 * r2;
+        println!("Total resistance {}", numerator / denominator);
     } else {
         println!("Bad input");
     }
-
-
-
-
 }
