ec3e7250205a1f0ca9590a2c24cd569a4febafeb
unknown <pepseman727@gmail.com>
2024-03-18 21:20:15 +0900

    Make solution simpler

--- task9/src/main.rs
+++ task9/src/main.rs
@@ -2,8 +2,6 @@ use std::io;
 
 fn main() {
     let mut resist = String::new();
-
-
     //Рассчётная формула 1/R = 1/R1 + 1/R2 + 1/R3
     // => R = R1R2R3/R2R3+R1R3+R1R2
     println!("Enter three resistance values separated by a space");
@@ -15,14 +13,11 @@ fn main() {
                             .split(' ')
                             .map(|value| value.to_string().parse::<f64>().expect("Enter a number"))
                             .collect();
-    if let [r1, r2, r3] = resist.as_slice() {
-        if *r1 < 0.0 || *r2 < 0.0 || *r3 < 0.0 {
-            println!("Bad input");
-        } else {
+    
+    if let [r1, r2, r3] = resist.clone().as_slice() {
             let numerator: f64 = r1 * r2 * r3;
             let denominator = r2 * r3 + r1 * r3 + r1 * r2;
             println!("Total resistance {}", numerator/denominator);
-        }
     } else {
         println!("Bad input");
     }
